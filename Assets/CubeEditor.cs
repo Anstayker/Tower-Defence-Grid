52e2f2b0b38378da272bca0746ba921817520887
Anstayker <Christian.rojas.bl@gmail.com>
2020-04-23 14:20:11 -0400

    Cube Editor added

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField][Range(1.0f, 20.0f)] float gridSize = 10.0f;
+
+    TextMesh textMesh;
 
     private void Update() {
         Vector3 snapPosition;
@@ -13,6 +16,9 @@ public class EditorSnap : MonoBehaviour {
         snapPosition.z = Mathf.RoundToInt(transform.position.z/gridSize) * gridSize;
 
         transform.position = new Vector3(snapPosition.x, 0, snapPosition.z);
+    
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPosition.x/gridSize + ", " + snapPosition.z/gridSize;
     }
 
 }
