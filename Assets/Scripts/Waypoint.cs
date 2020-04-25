33d55271dccb4f502b67cb4d8ff9b38f8cbb9bc4
Anstayker <Christian.rojas.bl@gmail.com>
2020-04-25 16:06:41 -0400

    Tower placement

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
 
-    public bool isExplored = false;
-    public Waypoint exploredFrom;
+    [HideInInspector] public bool isExplored = false;
+    [HideInInspector] public Waypoint exploredFrom;
+    public bool isPlaceable = true;
+    [Tooltip("Tower Prefab")][SerializeField] Tower tower;
 
     const int gridSize = 10;
     Vector2Int gridPos;
@@ -24,6 +26,14 @@ public class Waypoint : MonoBehaviour {
     public void SetTopColor(Color color) {
         MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
         topMeshRenderer.material.color = color;
-    }   
+    }
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0) && isPlaceable) {
+            Instantiate(tower, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
+    }
+   
 
 }
