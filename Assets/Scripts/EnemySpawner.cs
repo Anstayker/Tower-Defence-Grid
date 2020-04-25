4d117010de7564e61f2f837ace171da1b929aef0
Anstayker <Christian.rojas.bl@gmail.com>
2020-04-25 02:07:15 -0400

    Enemy Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] EnemyMovement enemy;
+    [SerializeField] float spawnLapse = 2.0f;
+
+    private void Start() {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy() {
+        while(true) {
+            Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnLapse);
+        }
+    }
+}
